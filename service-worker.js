e5e28981acf0a05c0bf4528c1c22440aef713af6
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-04-08 23:13:52 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE = "essence-v2";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll([
+      "/",
+      "/index.html",
+      "/style.css",
+      "/app.js"
+    ]))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
